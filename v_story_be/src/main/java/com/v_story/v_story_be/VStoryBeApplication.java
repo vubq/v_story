9ddec765e21ec32e4c576f51165f5013cdac5956
vubq <vubq242002@gmail.com>
2022-12-28 15:22:13 -0800

    Create Project BE

---
+++ v_story_be/src/main/java/com/v_story/v_story_be/VStoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.v_story.v_story_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VStoryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VStoryBeApplication.class, args);
+    }
+
+}
---
+++ v_story_be/src/test/java/com/v_story/v_story_be/VStoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v_story.v_story_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VStoryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
